1e7970d58865f7370bcf24d30ffd0b943892b25c
Jacky Fan <contact@jacky.fan>
2023-04-21 13:37:58 +0800

    fix: open url in new tab instead of the same tab

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -21,7 +21,7 @@ export default function ProjectPage({project} : ProjectPageProps) {
 
     const LinkButton = ({title, link} : {title: string, link: string}) => {
         return (
-            <a className="btn btn-outline no-underline" href={link}>
+            <a className="btn btn-outline no-underline" href={link} target="_blank" rel="noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
                 </svg>
