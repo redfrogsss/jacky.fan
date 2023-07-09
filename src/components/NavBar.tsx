739a79415722869712c43d064375123fde461987
Jacky Fan <contact@jacky.fan>
2023-07-09 09:33:19 +0800

    fix: page title button not using <Link> tag

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,26 @@
+import { Link, useLocation } from "react-router-dom";
+
+const PageTitle = () => {
+    const currentPath = useLocation().pathname;
+    const isRoot = currentPath == "/";
+
+    if (isRoot) {
+        return <a className="btn btn-ghost normal-case text-xl" href="/#intro">
+            Jacky FAN's Website
+        </a>
+    } else {
+        return <Link className="btn btn-ghost normal-case text-xl" to="/">
+            Jacky FAN's Website
+        </Link>
+    }
+}
+
 export const NavBar = () => {
+
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl" href="/#intro">
-                    Jacky FAN's Website
-                </a>
+                <PageTitle />
             </div>
             <div className="invisible md:visible flex-none">
                 <ul className="menu menu-horizontal px-1">
